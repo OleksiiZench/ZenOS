0b178060ae93d70b394e1036cb43cfc73b8f29d2
OleksiiZench <olekzenchenko@gmail.com>
2026-08-19 16:15:26 +0300

    feat: add BoardConfig

---
+++ include/BoardConfig.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "drivers/DisplayConfig.h"
+
+namespace BoardConfig
+{
+    constexpr DisplayConfig DISPLAY_CONFIG = {
+        .pin_backlight = GPIO_NUM_46,
+        .pin_mosi      = GPIO_NUM_17,
+        .pin_clk       = GPIO_NUM_18,
+        .pin_cs        = GPIO_NUM_7,
+        .pin_dc        = GPIO_NUM_15,
+        .pin_rst       = GPIO_NUM_NC
+    };
+}
